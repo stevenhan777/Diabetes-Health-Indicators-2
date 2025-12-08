bb8043f2662adf119245a3bb5af08012acea5ff3
stevenhan777 <120138752+stevenhan777@users.noreply.github.com>
2025-12-08 08:28:38 -0500

    requirements and installation done

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .' # end of requirements.txt for setup.py
+def get_requirements(file_path:str)->List[str]: #file_path is str, returns a List
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines() # read the line
+        requirements=[req.replace("\n","") for req in requirements] # remove the new line \n
+
+        if HYPEN_E_DOT in requirements: # remove the hypen_e_dot
+            requirements.remove(HYPEN_E_DOT) 
+    
+    return requirements
+
+setup(
+name='Diabetes-Health_Indicators-2-Project',
+version='0.0.1',
+author='Steven',
+author_email='120138752+stevenhan777@users.noreply.github.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
